cee0debefff62b5b22202611fb8d53b9d875870e
s-minii <yun2044992@naver.com>
2023-09-01 22:21:27 +0900

    @PostConstruct, @PreDestroy 애노테이션

--- 1_basic/project_1/core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ 1_basic/project_1/core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -19,6 +19,13 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig{
 
+        /*
+         @Bean(initMethod = "init", destroyMethod = "close")
+         언제 사용하냐 ? : 외부 라이브러리를 초기화, 종료해야하는 경우
+         destroyMethod는 close랑 shutdown 이름의 메서드를 자동으로 호출해준다. (추론 기능이 있다.)
+         추론 기능을 수행하기 싫으면 destroyMethod ="" 처럼 빈 공백을 지정하면 된다.
+        */
+
         @Bean
         public NetworkClient networkClient(){
             NetworkClient networkClient = new NetworkClient();
