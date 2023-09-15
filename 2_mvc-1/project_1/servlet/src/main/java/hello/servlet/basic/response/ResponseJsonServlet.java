859581cb92da3d932308984e5d4a292f0a838974
s-minii <yun2044992@naver.com>
2023-09-15 23:29:58 +0900

    HTTP 응답데이터 (단순 텍스트, HTML, JSON)

---
+++ 2_mvc-1/project_1/servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,34 @@
+package hello.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ResponseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest reqyest, HttpServletResponse response) throws ServletException, IOException {
+        //Content-Type : application/json
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        //아래 정보를 json으로 바꿀꺼임.
+        HelloData helloData = new HelloData();
+        helloData.setUsername("yun");
+        helloData.setAge(24);
+
+        // {"username" : "yun", "age" : 24}
+        String result = objectMapper.writeValueAsString(helloData);
+        response.getWriter().write(result);
+    }
+}
