39784346fc2df9b626362853e07b817415ebd170
s-minii <yun2044992@naver.com>
2023-09-23 23:09:39 +0900

    Http request param - @RequestParam

--- 2_mvc-1/project_2/springmvc/src/main/java/hello/spirngmvc/basic/request/RequestParamController.java
+++ 2_mvc-1/project_2/springmvc/src/main/java/hello/spirngmvc/basic/request/RequestParamController.java
@@ -3,10 +3,13 @@ package hello.spirngmvc.basic.request;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,6 +22,57 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age){
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+
+    /**
+     *  int age는 안 됨.
+     *  -> why ? : null이 들어왔을 때 오류뜸.
+     *  그럼 어떻게 ? : Integer를 사용. -> integer는 객체형 변수라 null이 됨.
+     *
+     *  username에 빈 문자 넣으면 통과가 됨.
+     *  -> why ? : 빈 문자, 즉 ""로 인식함. 그래서 아래 나오는 default를 넣어줌으로써 해결!
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) {
+        log.info("username={}, age={}", username, age); return "ok";
+    }
+
+
+    // defaultValue를 기본값 : guest로 지정
+    // defaultValue는 빈 문자도 처리해준다. (null이랑 ""랑 같이 해결해준다.)
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age) {
+        log.info("username={}, age={}", username, age); return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
     }
 
 }
