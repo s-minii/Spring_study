7e3f5bb63c8b0cdb98f48d9e316510fc2b5633f2
s-minii <yun2044992@naver.com>
2023-09-24 22:31:12 +0900

    response - 정적리소스, 뷰 템플릿

---
+++ 2_mvc-1/project_2/springmvc/src/main/java/hello/spirngmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,27 @@
+package hello.spirngmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1(){
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+
+        return mav;
+    }
+
+    // 이 방식이 가장 적당하다!
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model){
+        model.addAttribute("data", "hello!");;
+        return "response/hello";
+    }
+
+
+}
