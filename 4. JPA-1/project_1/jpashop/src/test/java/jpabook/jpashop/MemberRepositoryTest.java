84537e5a9821feeaed910ebf321c107c3acf68e1
s-minii <yun2044992@naver.com>
2023-09-27 23:45:28 +0900

    엔티티 클래스 개발1

--- 4. JPA-1/project_1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ 4. JPA-1/project_1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,6 +1,5 @@
 package jpabook.jpashop;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
