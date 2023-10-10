f79fe2efb1ec512dd145bbf3102fb7e13ba01870
s-minii <yun2044992@naver.com>
2023-10-10 19:27:33 +0900

    회원 레포지토리 개발

--- 4. JPA-1/project_1/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ 4. JPA-1/project_1/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -1,13 +1,10 @@
 package jpabook.jpashop.domain;
-
 import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
 import static javax.persistence.FetchType.*;
 
 @Entity
---
+++ 4. JPA-1/project_1/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    // 단건 조회
+    public Member findOne(Long id){
+        return em.find(Member.class, id); // id : pk
+    }
+
+    // 리스트 조회
+    public List<Member> findAll(){
+        List<Member> result = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        return result;
+    }
+
+    // 이름으로 조회
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
