6fd6d6be83a0a14ae2345dd2b8cc79784a6fa20b
s-minii <yun2044992@naver.com>
2023-10-11 21:05:53 +0900

    상품 엔터티 개발(비즈니스 로직 추가)

--- 4. JPA-1/project_1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ 4. JPA-1/project_1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,8 +22,47 @@ public abstract class Item {
 
     private String name;
     private int price;
-    private int stockQuantity;
+    private int stockQuantity; // 재고수량
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
