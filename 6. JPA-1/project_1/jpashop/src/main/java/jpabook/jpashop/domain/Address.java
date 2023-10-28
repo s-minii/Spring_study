24b707d33be44d7cb7d582a033534597ddd863f1
s-minii <yun2044992@naver.com>
2023-10-28 18:09:44 +0900

    엔티티 클래스 개발2

--- 6. JPA-1/project_1/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ 6. JPA-1/project_1/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,6 +6,8 @@ import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 
+
+// 값 타입은 기본적으로 변경 불가능하게 설계해야 한다. (Setter 설정X)
 @Embeddable // 내장될 수 있다.
 @Getter
 public class Address {
@@ -13,4 +15,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
