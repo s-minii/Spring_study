fcd13f0e904ff7e32decc3a75bbd217351fd303d
s-minii <yun2044992@naver.com>
2023-10-30 04:30:53 +0900

    주문 서비스 개발

--- 6. JPA-1/project_1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ 6. JPA-1/project_1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -26,6 +26,7 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
 
+
     //생성 메서드
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
         OrderItem orderItem = new OrderItem();
