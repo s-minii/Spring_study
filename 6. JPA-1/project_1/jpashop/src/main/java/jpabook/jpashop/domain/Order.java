3f1afc08f29805439c83de1df7b100bc40ec53cb
s-minii <yun2044992@naver.com>
2023-10-30 03:57:55 +0900

    주문, 주문상품 엔티티 개발

--- 6. JPA-1/project_1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ 6. JPA-1/project_1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,11 +1,16 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "orders")
 public class Order {
 
@@ -49,5 +54,45 @@ public class Order {
         delivery.setOrder(this);
     }
 
+    //생성 메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //비즈니스 로직
+    /**
+     * 주문 취소
+     */
+    public void cancel(){
+        if (delivery.getStatus() == DeliveryStatus.COMP){ // 배송 상태가 이미 배송완료인 경우
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL); // 위 로직을 통과한 경우 이 로직을 실행한다.
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //조회 로직
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice(){
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += getTotalPrice();
+        }
+        return totalPrice;
+    }
 
 }
--- 6. JPA-1/project_1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ 6. JPA-1/project_1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -26,4 +26,29 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
 
+    //생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count); // 주문이 들어오면 그 개수만큼 재고를 줄인다.
+        return orderItem;
+    }
+
+
+
+    // 비즈니스 로직
+    /**
+     *  Order에서 주문취소를 구현하는 도중 구현함
+     *  고객이 주문을 한 번 할때, 상품을 2개 이상 주문 했을 경우 취소도 그 개수 만큼 늘려야 하는 경우
+     */
+    public void cancel(){
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice(){
+        return getOrderPrice() * getCount();
+    }
 }
