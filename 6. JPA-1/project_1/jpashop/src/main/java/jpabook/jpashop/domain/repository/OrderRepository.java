592d03886a073f60faa7beaf8bd65493438cd5be
s-minii <yun2044992@naver.com>
2023-10-30 04:03:36 +0900

    주문 리포지토리 개발

---
+++ 6. JPA-1/project_1/jpashop/src/main/java/jpabook/jpashop/domain/repository/OrderRepository.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    //단건 조회
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    // 회원명, 주문상태로 검색하기
+    // public List<Order> findAll(){}
+}
